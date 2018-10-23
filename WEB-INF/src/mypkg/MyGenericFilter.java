a662ff36bc991e7c109845cfc4286e104347f0e8
Eric <42637403+ericliu1819@users.noreply.github.com>
2018-10-23 17:09:29 +0800

    Update WEB-INF/src/mypkg/MyGenericFilter.java

---
+++ WEB-INF/src/mypkg/MyGenericFilter.java
@@ -0,0 +1,20 @@
+package mypkg;
+import javax.servlet.*;
+
+public class MyGenericFilter implements javax.servlet.Filter {
+  public FilterConfig filterConfig;                                 //1
+
+  public void doFilter(final ServletRequest request,                //2
+                       final ServletResponse response,
+                       FilterChain chain)
+      throws java.io.IOException, javax.servlet.ServletException { 
+    chain.doFilter(request,response);                               //3
+  } 
+
+  public void init(final FilterConfig filterConfig) {               //4
+    this.filterConfig = filterConfig;
+  } 
+
+  public void destroy() {                                           //5
+  }
+}
