6017361f5499f6a8c03b7820418de5ff3e058498
Eric <42637403+ericliu1819@users.noreply.github.com>
2018-10-24 16:12:25 +0800

    Update WEB-INF/src/mypkg/HelloWorldFilter.java

--- WEB-INF/src/mypkg/HelloWorldFilter.java
+++ WEB-INF/src/mypkg/HelloWorldFilter.java
@@ -9,7 +9,7 @@ public class HelloWorldFilter extends MyGenericFilter {
                        final ServletResponse response,
                        FilterChain chain)
        throws java.io.IOException, javax.servlet.ServletException  { 
-    System.out.println("Entering Filter");
+    System.out.println("Entering HelloWorldFilter");
     request.setAttribute("hello","Hello World!");
     chain.doFilter(request,response);
     System.out.println("Exiting HelloWorldFilter"); 
